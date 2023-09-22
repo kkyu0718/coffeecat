af0c00ea56149fe8d99f3b1201ed3d3c45ed6af0
kkyu0718 <net605@naver.com>
2023-09-22 21:12:59 +0900

    init

---
+++ src/main/java/com/coffeecat/coffeecat/CoffeecatApplication.java
@@ -0,0 +1,13 @@
+package com.coffeecat.coffeecat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeecatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeecatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coffeecat/coffeecat/CoffeecatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeecat.coffeecat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeecatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
