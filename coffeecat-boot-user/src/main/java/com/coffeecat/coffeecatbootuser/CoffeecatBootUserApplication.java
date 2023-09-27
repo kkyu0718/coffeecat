9b23e85558d67c1cfde1403ef45478e6edb53252
kkyu0718 <net605@naver.com>
2023-09-28 01:49:19 +0900

    set multimodule

---
+++ coffeecat-boot-user/src/main/java/com/coffeecat/coffeecatbootuser/CoffeecatBootUserApplication.java
@@ -0,0 +1,13 @@
+package com.coffeecat.coffeecatbootuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeecatBootUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeecatBootUserApplication.class, args);
+	}
+
+}
---
+++ coffeecat-data-user/src/test/java/com/coffeecat/coffeecatdatauser/CoffeecatDataUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeecat.coffeecatdatauser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeecatDataUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
